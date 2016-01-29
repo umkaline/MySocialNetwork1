0c15f08df1c7f9c830c82ab5211b27b015ebaecc
umkaline <glmax132@gmail.com>
2016-01-29 23:33:11 +0200

    123

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
-process.env.DB_HOST = 'localhost';
-process.env.DB_PORT = '27017';
-process.env.DB_NAME = 'VRakashy';
-process.env.DB_USER = '';
-process.env.DB_PASS = '';
+process.env.DB_HOST = 'ds051645.mongolab.com';
+process.env.DB_PORT = '51645';
+process.env.DB_NAME = 'heroku_4hsq932r';
+process.env.DB_USER = 'admin';
+process.env.DB_PASS = 'max123';
